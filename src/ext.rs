ec61baeaeba339d3c0777ee6252282c142b59e32
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-22 02:33:21 -0400

    Remove MemEq restriction on Self
    Any sized type now can call mem_eq on another sized type.

--- src/ext.rs
+++ src/ext.rs
@@ -1,10 +1,12 @@
 use core::mem::size_of;
+use core::cmp;
 
 // External C functions are declared here to avoid depending on libc.
 extern "C" {
     pub fn memcmp(s1: *const i8, s2: *const i8, n: usize) -> i32;
 }
 
-pub unsafe fn _memcmp<T>(x: *const T, y: *const T) -> i32 {
-    memcmp(x as _, y as _, size_of::<T>())
+pub unsafe fn _memcmp<T, U>(x: *const T, y: *const U) -> i32 {
+    let size = cmp::min(size_of::<T>(), size_of::<U>());
+    memcmp(x as _, y as _, size)
 }
--- src/mem_eq.rs
+++ src/mem_eq.rs
@@ -1,17 +1,36 @@
+use core::mem::size_of;
 use ext::*;
 
 /// Trait for equality comparisons performed over bytes directly.
-pub trait MemEq {
+pub trait MemEq<Rhs: ?Sized = Self> {
     /// Tests whether `self` and `other` are equal in memory.
-    fn mem_eq(&self, other: &Self) -> bool;
+    fn mem_eq(&self, other: &Rhs) -> bool;
 
     /// Tests whether `self` and `other` are not equal in memory.
     #[inline]
-    fn mem_neq(&self, other: &Self) -> bool { !self.mem_eq(other) }
+    fn mem_neq(&self, other: &Rhs) -> bool { !self.mem_eq(other) }
 }
 
-impl<T> MemEq for T {
-    fn mem_eq(&self, other: &Self) -> bool {
-        unsafe { _memcmp(self, other) == 0 }
+impl<T, U> MemEq<U> for T {
+    fn mem_eq(&self, other: &U) -> bool {
+        size_of::<T>() == size_of::<U>() && unsafe { _memcmp(self, other) == 0 }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn equal_bytes() {
+        let buf = [0u8; 128];
+        assert!(buf.mem_eq(&buf));
+
+        let x = [0u8; 1];
+        let y = 0u8;
+        assert!(x.mem_eq(&y));
+
+        assert!(buf.mem_neq(&x));
+        assert!(buf.mem_neq(&y));
     }
 }
