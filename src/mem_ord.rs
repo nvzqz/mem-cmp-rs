847b40cad8d5a75878e438b00309bccc7781c85d
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-22 06:21:20 -0400

    Remove _mem_cmp

--- src/mem_ord.rs
+++ src/mem_ord.rs
@@ -18,34 +18,27 @@ fn convert(cmp: i32, size_a: usize, size_b: usize) -> Ordering {
     }
 }
 
-#[inline(always)]
-fn _mem_cmp<T, U>(this: &T, other: &U) -> Ordering {
-    use self::mem::{size_of, transmute_copy};
-    macro_rules! impl_match {
-        ($($s:expr, $t:ty);+) => {
-            match (size_of::<T>(), size_of::<U>()) {
-                $(($s, $s) => unsafe {
-                    let x: $t = transmute_copy(this);
-                    let y: $t = transmute_copy(other);
-                    x.cmp(&y)
-                },)+
-                _ => {
-                    let cmp = unsafe { _memcmp(this, other, 1) };
-                    convert(cmp, size_of::<T>(), size_of::<U>())
+impl<T, U> MemOrd<U> for T {
+    #[inline]
+    fn mem_cmp(&self, other: &U) -> Ordering {
+        use self::mem::{size_of, transmute_copy};
+        macro_rules! impl_match {
+            ($($s:expr, $t:ty);+) => {
+                match (size_of::<T>(), size_of::<U>()) {
+                    $(($s, $s) => unsafe {
+                        let x: $t = transmute_copy(self);
+                        let y: $t = transmute_copy(other);
+                        x.cmp(&y)
+                    },)+
+                    _ => {
+                        let cmp = unsafe { _memcmp(self, other, 1) };
+                        convert(cmp, size_of::<T>(), size_of::<U>())
+                    }
                 }
             }
         }
+        impl_match!(1, u8; 2, u16; 4, u32; 8, u64)
     }
-    impl_match!(1, u8; 2, u16; 4, u32; 8, u64)
-}
-
-pub fn compare_arrays(a: [u8; 0], b: [u16; 0]) -> Ordering {
-    a.mem_cmp(&b)
-}
-
-impl<T, U> MemOrd<U> for T {
-    #[inline]
-    fn mem_cmp(&self, other: &U) -> Ordering { _mem_cmp(self, other) }
 }
 
 impl<T, U> MemOrd<[U]> for [T] {
@@ -69,20 +62,6 @@ mod tests {
         let x = [0u8, 0, 0, 0];
         let y = [0u8, 0, 0, 4];
         assert_eq!(x.cmp(&y), x.mem_cmp(&y));
-    }
-
-    #[test]
-    fn compare_signed() {
-        macro_rules! compare {
-            ($x:expr, $y:expr, $($t:ty),+) => {
-                $({
-                    let x: $t = $x;
-                    let y: $t = $y;
-                    assert_eq!(x.mem_cmp(&y), _mem_cmp(&x, &y));
-                })+
-            }
-        }
-        compare!(-1, 1, i8, i16, i32, i64, isize);
     }
 
     #[test]
