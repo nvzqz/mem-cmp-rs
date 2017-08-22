e9b0c18aab83643a1ca572e606e58b1f47f04d6e
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-22 03:07:56 -0400

    Move MemOrdered out of crate root

--- src/lib.rs
+++ src/lib.rs
@@ -3,55 +3,8 @@
 mod ext;
 mod mem_eq;
 mod mem_ord;
-
-use core::cmp::Ordering;
-use core::mem;
+mod mem_ordered;
 
 pub use mem_eq::*;
 pub use mem_ord::*;
-
-/// A type that implements comparison traits via [`MemEq`](trait.MemEq.html)
-/// and [`MemOrd`](trait.MemOrd.html).
-pub struct MemOrdered<T: ?Sized>(pub T);
-
-impl<T> From<T> for MemOrdered<T> {
-    fn from(inner: T) -> Self { MemOrdered(inner) }
-}
-
-impl<'a, T: ?Sized> From<&'a T> for &'a MemOrdered<T> {
-    fn from(inner: &T) -> Self { unsafe { mem::transmute(inner) } }
-}
-
-impl<'a, T: ?Sized> From<&'a mut T> for &'a mut MemOrdered<T> {
-    fn from(inner: &mut T) -> Self { unsafe { mem::transmute(inner) } }
-}
-
-impl<T: ?Sized> AsRef<T> for MemOrdered<T> {
-    fn as_ref(&self) -> &T { &self.0 }
-}
-
-impl<T: ?Sized> AsMut<T> for MemOrdered<T> {
-    fn as_mut(&mut self) -> &mut T { &mut self.0 }
-}
-
-impl<T: ?Sized, U: ?Sized> PartialEq<MemOrdered<U>> for MemOrdered<T>
-    where T: MemEq<U>
-{
-    fn eq(&self, other: &MemOrdered<U>) -> bool {
-        self.0.mem_eq(&other.0)
-    }
-}
-
-impl<T> Eq for MemOrdered<T> {}
-
-impl<T: ?Sized + MemOrd> PartialOrd for MemOrdered<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.0.mem_cmp(&other.0))
-    }
-}
-
-impl<T: MemOrd> Ord for MemOrdered<T> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.0.mem_cmp(&other.0)
-    }
-}
+pub use mem_ordered::*;
